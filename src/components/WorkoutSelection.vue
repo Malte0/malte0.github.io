a9d005d85c5f4b36493c330d46a57d553f905a60
Malte Wagenknecht <wagenknecht.malte@gmail.com>
2025-11-04 23:12:02 +0100

    minor progress

--- src/components/WorkoutSelection.vue
+++ src/components/WorkoutSelection.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref, type Ref } from 'vue'
 import NotFound from './NotFound.vue'
+import ProgressInput from './ProgressInput.vue'
+
+const splitDays: Ref<string[]> | Ref<null> = ref(null)
+  onMounted(async () => {
+    try {
+      const response = await fetch(`/workouts/split.json`)
+      splitDays.value = await response.json()
+      console.log(splitDays.value)
+    } catch (error) {
+      console.error('Error loading workout data:', error)
+    }
+  })
 
 const routes: { [type: string]: any } = {
-  '/': WorkoutSelection,
+  '/': ProgressInput, // somehow get to progress input with the first exercise
 }
+
 
 const currentPath = ref(window.location.hash)
 
@@ -21,10 +34,27 @@ const currentView = computed(() => {
 <template>
   <div>
     <h2>Workout Selection Component</h2>
-    <button >Legs</button>
+    <ul id="listContainer">
+      <li class="workout-button" v-for="(day, index) in splitDays" :key="index">
+        <a :href="`${day}`">{{ day }}</a> 
+        <!-- Link to each workout day -->
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
+#listContainer {
+  padding: 0;
+}
 
+.workout-button {
+  list-style-type: none;
+  margin: 10px 0;
+  padding: 10px;
+  background-color: #ffffff;
+  color: rgb(0, 0, 0);
+  text-align: center;
+  border-radius: 5px;
+}
 </style>
