aba696b9d02f1ba48094da7859a7878cc8dae576
Malte Wagenknecht <wagenknecht.malte@gmail.com>
2026-05-20 14:43:57 +0200

    setup login

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import ExerciseInput from "./views/ExerciseInputView.vue";
+import LoginView from "./views/LoginView.vue";
 
 const routes = [
   { path: "/", component: ExerciseInput },
   { path: "/input", component: ExerciseInput },
+  { path: "/login", component: LoginView },
+  { path: "/rawData", component: ExerciseInput },
   // { TODO: put in seperate view for tidyness
   //   path: "material-editor/:pathMatch(.*)",
   //   component: CaseView,
